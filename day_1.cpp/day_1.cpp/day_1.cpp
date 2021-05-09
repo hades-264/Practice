1b81e3c9a419cfb7bf27f9a01a571864982b7e46
HRITIK DETHALIYA <hritik14092000@gmail.com>
2021-05-09 23:27:42 +0530

    may challenge  1st problem

---
+++ day_1.cpp/day_1.cpp/day_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() { int t;
+cin>>t;
+int a,b,x;
+while(t-- ){
+cin>>x >>a >>b;
+int s =a+(b*(100-x ));
+cout<<s*10<<endl ;}
+	// your code goes here
+	return 0;
+}
