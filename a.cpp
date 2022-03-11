344e58599656676658f06d6b9d2e5b58b77e396b
Hritik Dethaliya <hellocodegit00@gmail.com>
2022-03-11 17:54:14 +0530

    cpp file

---
+++ a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define line "\n"
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void code()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    
+
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        code();
+    }
+    return 0;
+}
